1dda8a067aae85f05fffbddf35d1d0b1fce77f41
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-05-11 16:08:29 -0400

    update

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ var shop = [
   
   
     for (var i=0; i < shop.length; i++){
-        var heading = '<div class="product col-sm-6 col-md-4 col-lg-3">';
+        var heading = '<div class="product col-sm-6 col-md-4 col-lg-3 col-2 mt-5">';
         var image = '<img class="img-fluid mb-4" src="' + shop[i].image + '"/';
         var price = '<div><p>' + '<span class="price"> $' + shop[i].price + '</span>';
         var description = shop[i].description + '</p><a class="cart" href="#"><i class="fas fa-cart-plus"></i></a> <a class="view" href="#" target="_blank"> View Item</a></div></div>';
